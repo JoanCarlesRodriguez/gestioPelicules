9584078bf71b9bcd5b27168af42f7408c44cc236
JoanCarlesRodriguez <joancarles.rs@gmail.com>
2021-11-30 16:27:57 +0100

    Revisat per funcionar a Casa

--- src/main/java/Dades/Conexio.java
+++ src/main/java/Dades/Conexio.java
@@ -14,12 +14,12 @@ public class Conexio {
     public static Connection conectar() throws SQLException, ClassNotFoundException {
         String sDriver = "com.mysql.cj.jdbc.Driver";
         String url = "jdbc:mysql://10.100.64.184/prova";          //Classe
-        String url2 = "jdbc:mysql://192.168.1.9/prova";    //Casa*/
+        String url2 = "jdbc:mysql://192.168.1.13/prova";    //Casa*/
         String user = "admin";
         String pass = "12345678";
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-        return DriverManager.getConnection(url, user, pass);
+        return DriverManager.getConnection(url2, user, pass);
     }
     public static void desconectar(Connection con, Statement sta) throws SQLException {
         sta.close();
